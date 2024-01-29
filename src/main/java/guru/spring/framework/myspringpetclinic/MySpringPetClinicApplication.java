afa666da5ea0637102d8aac5e516d75d267b61ec
catalin.fosalau <fosalaucatalin@gmail.com>
2024-01-29 17:01:52 +0200

    Initial Project Add

---
+++ src/main/java/guru/spring/framework/myspringpetclinic/MySpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.spring.framework.myspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringPetClinicApplication.class, args);
+	}
+
+}
