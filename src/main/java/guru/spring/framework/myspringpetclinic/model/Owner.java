00b70b3a3db0fcbe1aa89a8d7d0d969b228332a4
catalin.fosalau <fosalaucatalin@gmail.com>
2024-01-30 14:36:19 +0200

    Adding POJOs. Closes #2

---
+++ src/main/java/guru/spring/framework/myspringpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package guru.spring.framework.myspringpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/guru/spring/framework/myspringpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package guru.spring.framework.myspringpetclinic.model;
+
+public class Vet extends Person {
+
+}
