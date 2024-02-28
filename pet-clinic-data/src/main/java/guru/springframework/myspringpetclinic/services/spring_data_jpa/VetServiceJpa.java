dea38e28780a56051b1310bef1804b6e9af93219
catalin.fosalau <fosalaucatalin@gmail.com>
2024-02-28 18:28:38 +0200

    Create Spring Data JPA Service for Owners. Closes #41

---
+++ pet-clinic-data/src/main/java/guru/springframework/myspringpetclinic/services/spring_data_jpa/VetServiceJpa.java
@@ -0,0 +1,47 @@
+package guru.springframework.myspringpetclinic.services.spring_data_jpa;
+
+import guru.springframework.myspringpetclinic.model.Vet;
+import guru.springframework.myspringpetclinic.repositories.VetRepository;
+import guru.springframework.myspringpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetServiceJpa implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetServiceJpa(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> owners = new HashSet<>();
+        vetRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
