b62955729098b73a88ad7b70f986b742434239d7
catalin.fosalau <fosalaucatalin@gmail.com>
2024-02-28 12:35:37 +0200

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/myspringpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.myspringpetclinic.services;
+
+import guru.springframework.myspringpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
