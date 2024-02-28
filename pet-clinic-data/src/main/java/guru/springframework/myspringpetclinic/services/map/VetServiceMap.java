920bb57225c7283f28cc56d60f2007c9607ca103
catalin.fosalau <fosalaucatalin@gmail.com>
2024-02-28 13:33:53 +0200

    Adding specialties to vets on startup. Closes #34

--- pet-clinic-data/src/main/java/guru/springframework/myspringpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/myspringpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package guru.springframework.myspringpetclinic.services.map;
 
+import guru.springframework.myspringpetclinic.model.Specialty;
 import guru.springframework.myspringpetclinic.model.Vet;
+import guru.springframework.myspringpetclinic.services.SpecialtyService;
 import guru.springframework.myspringpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,13 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -20,6 +29,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (!object.getSpecialties().isEmpty()) {
+            object.getSpecialties().forEach(specialty -> {
+                if (specialty.getId() == null) {
+                    Specialty savedSpecialty = specialtyService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
+
         return super.save(object);
     }
 
